d0c6981ede1d499fbbc4aabeda3715153c5c522c
juansantosgomez <juansantosgomez@protonmail.com>
2023-08-12 06:56:07 +0800

    version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = "jsgprogressbar",
-    version = "1.0.0",
+    version = "1.0.1",
     author = "Juan Santos Gomez",
     description="A function that makes a progressbar for your loops"
 )
