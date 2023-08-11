e4927b4f5fb8464668c201deaa3747e23172bbbe
juansantosgomez <juansantosgomez@protonmail.com>
2023-08-12 06:41:47 +0800

    init dunder and setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name = "loopprogress"
+    version = "1.0.0"
+    author = "Juan Santos Gomez"
+    description="A function that makes a progressbar for your loops"
+)
