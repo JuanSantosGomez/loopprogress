9e4de7bd696aedfb2b9318427398bd78a7508e06
juansantosgomez <juansantosgomez@protonmail.com>
2023-08-12 05:54:25 +0800

    Define progress bar function

---
+++ progressbar.py
@@ -0,0 +1,12 @@
+from sys import stdout
+def progressbar(it, prefix="", size=60, out=stdout): # Python3.3+
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        print("{}[{}{}] {}/{}".format(prefix, "█"*x, "."*(size-x), j, count), 
+                end='\r', file=out, flush=True)
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    print("\n", flush=True, file=out)
