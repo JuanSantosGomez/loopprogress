bbd5a39cb4fd6e4d8a2e0e28d3b52ba709eb707e
juansantosgomez <juansantosgomez@protonmail.com>
2023-08-12 09:02:09 +0800

    improved colors

--- jsgprogressbar/progressbar.py
+++ jsgprogressbar/progressbar.py
@@ -6,6 +6,7 @@ class bcolors:
     OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
+    GRAY = '\033[0;90m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
@@ -16,7 +17,7 @@ def progressbar(it, prefix="", size=60, out=stdout): # Python3.3+
     count = len(it)
     def show(j):
         x = int(size*j/count)
-        print("{}{}{}{}[{}{}] {}{}/{}".format(bcolors.BOLD,prefix,bcolors.ENDC,bcolors.WARNING, "█"*x, "."*(size-x),bcolors.ENDC, j, count), 
+        print("{}{}{}{}|{}{}{}{}|{} {}/{}".format(bcolors.BOLD,prefix,bcolors.ENDC,bcolors.WARNING, "█"*x,bcolors.ENDC, bcolors.GRAY+"█"*(size-x)+bcolors.ENDC,bcolors.WARNING,bcolors.ENDC, j, count), 
                 end='\r', file=out, flush=True)
         
         
