be21710ca8e02efb0f5104a82a2f0687db192edb
juansantosgomez <juansantosgomez@protonmail.com>
2023-08-17 00:02:35 +0800

    bump to 1.0.4

--- jsgprogressbar/progressbar.py
+++ jsgprogressbar/progressbar.py
@@ -1,16 +1,11 @@
 from sys import stdout
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     GRAY = '\033[0;90m'
-    FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+
 
 
 def progressbar(it, prefix="", size=60, out=stdout): # Python3.3+
